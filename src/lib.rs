7057c89ae44d1b9f02bed22e00beb94891729b1a
Joshua Abraham <sinisterpatrician@gmail.com>
2020-04-06 13:04:20 -0400

    Use tls_parser to process client hello

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,12 @@ use pcap::Capture;
 use pnet::packet::*;
 use pnet::packet::ethernet::EtherType;
 use pnet::packet::ip::IpNextHeaderProtocol;
+use tls_parser::parse_tls_plaintext;
+use tls_parser::tls::{TlsRecordType, TlsMessage, TlsMessageHandshake};
 
 // curl ja3 hash: 456523fc94726331a4d5a2e1d40b2cd7
 // firefox ja3 hash: 839bbe3ed07fed922ded5aaf714d6842
+// "771,49195-49199-52393-52392-49196-49200-49162-49161-49171-49172-51-57-47-53-10,0-23-65281-10-11-35-16-5-13-28,29-23-24-25,0"
 
 lazy_static! {
     static ref IPTYPE: IpNextHeaderProtocol = IpNextHeaderProtocol::new(6);
@@ -22,6 +25,36 @@ pub enum Error {
 type Result<T> = std::result::Result<T, Error>;
 
 pub fn ja3_hash_client_hello(packet: &[u8]) -> Option<Digest> {
+    let mut ja3_string = String::new();
+    let res = parse_tls_plaintext(packet);
+    match res {
+        Ok((rem, record)) => {
+            eprintln!("Rem: {:?}, record: {:?}", rem, record);
+            eprintln!("record type: {:?}", record.hdr.record_type);
+            if record.hdr.record_type != TlsRecordType::Handshake {
+                return None;
+            }
+            for rec in record.msg {
+                if let TlsMessage::Handshake(handshake) = rec {
+                    if let TlsMessageHandshake::ClientHello(contents) = handshake {
+                        eprintln!("handshake contents: {:?}", contents);
+                        eprintln!("handshake tls version: {:?}", u16::from(contents.version));
+                        ja3_string.push_str(&format!("{},", u16::from(contents.version)));
+                        for cipher in contents.ciphers {
+                            eprintln!("handshake cipher: {}", u16::from(cipher));
+                            ja3_string.push_str(&format!("{}-", u16::from(cipher)));
+                        }
+                        ja3_string.pop();
+                    }
+                }
+            }
+        },
+        _ => {
+            eprintln!("ERROR");
+        },
+    }
+
+    eprintln!("ja3_string: {}", ja3_string);
     None
 }
 
@@ -53,10 +86,11 @@ pub fn process_pcap<P: AsRef<Path>>(pcap_path: P) -> Result<()> {
         eprintln!("handshake_start: {}", handshake_start);
         let handshake = &packet[handshake_start..];
         eprintln!("handshake: {:x?}", handshake);
-        if handshake[0] != 0x22 {
+        if handshake[0] != 0x16 {
             continue;
         }
 
+        eprintln!("sending handshake");
         let digest = ja3_hash_client_hello(&handshake);
     }
 
