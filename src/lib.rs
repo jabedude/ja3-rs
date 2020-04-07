e13d9b990e01fe2c4b0a440aa4abe48392726cef
Joshua Abraham <sinisterpatrician@gmail.com>
2020-04-07 14:46:35 -0400

    Add test for ipv6 support

--- src/lib.rs
+++ src/lib.rs
@@ -188,11 +188,22 @@ mod tests {
 
     #[test]
     fn test_ja3_curl_full_stream() {
-        env_logger::init();
         let expected_str = "771,4866-4867-4865-49196-49200-159-52393-52392-52394-49195-49199-158-49188-49192-107-49187-49191-103-49162-49172-57-49161-49171-51-157-156-61-60-53-47-255,0-11-10-13172-16-22-23-13-43-45-51-21,29-23-30-25-24,0-1-2";
         let expected_hash = "456523fc94726331a4d5a2e1d40b2cd7";
 
         let mut ja3s = process_pcap("curl.pcap").unwrap();
+        let ja3 = ja3s.pop().unwrap();
+        assert_eq!(ja3.ja3_str, expected_str);
+        assert_eq!(format!("{:x}", ja3.hash), expected_hash);
+    }
+
+    #[test]
+    fn test_ja3_curl_full_stream_ipv6() {
+        env_logger::init();
+        let expected_str = "771,4866-4867-4865-49196-49200-159-52393-52392-52394-49195-49199-158-49188-49192-107-49187-49191-103-49162-49172-57-49161-49171-51-157-156-61-60-53-47-255,0-11-10-13172-16-22-23-13-43-45-51-21,29-23-30-25-24,0-1-2";
+        let expected_hash = "456523fc94726331a4d5a2e1d40b2cd7";
+
+        let mut ja3s = process_pcap("curl-ipv6.pcap").unwrap();
         let ja3 = ja3s.pop().unwrap();
         assert_eq!(ja3.ja3_str, expected_str);
         assert_eq!(format!("{:x}", ja3.hash), expected_hash);
