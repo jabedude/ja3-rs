ec3cd35b408db2e082fae151f186be7edf7bb67d
Joshua Abraham <sinisterpatrician@gmail.com>
2020-04-13 11:11:47 -0400

    Add simple live capture example

---
+++ examples/stream.rs
@@ -0,0 +1,8 @@
+use ja3::Ja3;
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let mut ja3 = Ja3::new(&args[1])
+                        .process_live(|x| println!("{}", x))
+                        .unwrap();
+}
