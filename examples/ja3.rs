a05f747e69bbc03aa4daef53c64535413fb4df06
Joshua Abraham <sinisterpatrician@gmail.com>
2020-04-13 11:31:41 -0400

    Fix build warnings

--- examples/ja3.rs
+++ examples/ja3.rs
@@ -2,9 +2,9 @@ use ja3::Ja3;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let mut ja3 = Ja3::new(&args[1])
-                        .process_pcap()
-                        .unwrap();
+    let ja3 = Ja3::new(&args[1])
+                    .process_pcap()
+                    .unwrap();
 
     for hash in ja3 {
         println!("{}", hash);
--- examples/stream.rs
+++ examples/stream.rs
@@ -2,7 +2,7 @@ use ja3::Ja3;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let mut ja3 = Ja3::new(&args[1])
-                        .process_live(|x| println!("{}", x))
-                        .unwrap();
+    let ja3 = Ja3::new(&args[1])
+                    .process_live(|x| println!("{}", x))
+                    .unwrap();
 }
